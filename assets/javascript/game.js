799e832a729a24dda9b5958859c60a0cbb93286a
Morpheus <masai.nazaire@gmail.com>
2019-06-13 10:12:31 -0400

    almost complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
-var gamesList = ["tony hawks pro skater", "grand theft auto", "resident evil",  "metal gear solid",  "san andreas",
-	"vice city", "gran turismo",  "madden nfl", "devil may cry", "ncaa football",  "god of war",
-	"virtua fighter",  "burnout",  "ssx",  "okami", "espn nfl", "soul calibur",  "prince of persia", "final fantasy", 
-	"guitar hero", "ratchet and clank", "jak and daxter", "twisted metal", "shadow of the colossus",
-	"nba street",   "splinter cell", "dragon quest", "tekken",  "sly cooper", "socom", "dark cloud", 
-	"kingdom hearts", "beyond good and evil", "suikoden", "amplitude", "psychonauts", "viewtiful joe",
-	"psi ops", "Red Faction", "onimusha", "need for speed", "mercenaries", "hot shots golf", "rogue galaxy",
-	"def jam vendetta", "wipeout", "xmen legends", "tomb raider", "mortal kombat", "legacy of kain",
-	"max payne", "war of the monsters"
+var gamesList = ["tonyhawksproskater", "grandtheftauto", "residentevil", "metalgearsolid", "sanandreas",
+    "vicecity", "granturismo", "maddennfl", "devilmaycry", "ncaafootball", "godofwar",
+    "virtuafighter", "burnout", "ssx", "okami", "espnnfl", "soulcalibur", "princeofpersia", "finalfantasy",
+    "guitarhero", "ratchetandclank", "jakanddaxter", "twistedmetal", "shadowofthecolossus",
+    "nbastreet", "splintercell", "dragonquest", "tekken", "slycooper", "socom", "darkcloud",
+    "kingdomhearts", "beyondgoodandevil", "suikoden", "amplitude", "psychonauts", "viewtifuljoe",
+    "psiops", "redfaction", "onimusha", "needforspeed", "mercenaries", "hotshotsgolf", "roguegalaxy",
+    "defjamvendetta", "wipeout", "xmenlegends", "tombraider", "mortalkombat", "legacyofkain",
+    "maxpayne", "warofthemonsters"
 ];
 
 var gameChose = "";
@@ -21,3 +21,92 @@ var wrongs = [];
 
 var wins = 0;
 var losses = 0;
+var numGuesses = 10;
+
+function startGame() {
+    numGuesses = 10;
+
+    gameChose = gamesList[Math.floor(Math.random() * gamesList.length)];
+
+    lettersInWord = gameChose.split("");
+
+    blanks = lettersInWord.length;
+
+    console.log(gameChose);
+
+    blanksAndRights = [];
+
+    wrongs = [];
+
+    for (var i = 0; i < blanks; i++) {
+        blanksAndRights.push("_");
+    }
+
+    document.getElementById("guesses-left").innerHTML = numGuesses;
+
+    document.getElementById("word-guess").innerHTML = blanksAndRights.join(" ");
+
+    document.getElementById("wrongs").innerHTML = wrongs.join(" ");
+}
+
+function letterCheck(letter) {
+
+    var lettersInWord = false;
+
+    for (var i = 0; i < blanks; i++) {
+        if (gameChose[i] === letter) {
+
+            lettersInWord = true;
+
+        }
+    }
+
+    if (lettersInWord) {
+        for (var x = 0; x < blanks; x++) {
+            if (gameChose[x] === letter) {
+                blanksAndRights[x] = letter;
+            }
+        }
+    }
+
+    else {
+        wrongs.push(letter);
+        numGuesses--;
+    }
+}
+
+
+
+function roundComplete() {
+    document.getElementById("guesses-left").innerHTML = numGuesses;
+    document.getElementById("word-guess").innerHTML = blanksAndRights.join(" ");
+    document.getElementById("wrongs").innerHTML = wrongs.join(" ");
+
+    if (lettersInWord.toString() === blanksAndRights.toString()) {
+        wins++;
+
+        document.getElementById("wins").innerHTML = wins;
+        startGame();
+    }
+
+    else if (numGuesses === 0) {
+        losses++;
+
+        document.getElementById("losses").innerHTML = losses;
+
+        startGame();
+
+    }
+}
+
+
+startGame();
+
+document.onkeyup = function (event) {
+
+    if (event.keyCode >= 65 && event.keyCode <= 90 ) {
+        var letterGuessed = event.key.toLowerCase();
+        letterCheck(letterGuessed);
+        roundComplete();
+    }
+};
