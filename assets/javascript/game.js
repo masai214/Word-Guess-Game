f2d49429fa3c31d0008211d6ab921240e73218b1
Morpheus <masai.nazaire@gmail.com>
2019-06-13 00:20:47 -0400

    background slideshow

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+var gamesList = ["tony hawks pro skater", "grand theft auto", "resident evil",  "metal gear solid",  "san andreas",
+	"vice city", "gran turismo",  "madden nfl", "devil may cry", "ncaa football",  "god of war",
+	"virtua fighter",  "burnout",  "ssx",  "okami", "espn nfl", "soul calibur",  "prince of persia", "final fantasy", 
+	"guitar hero", "ratchet and clank", "jak and daxter", "twisted metal", "shadow of the colossus",
+	"nba street",   "splinter cell", "dragon quest", "tekken",  "sly cooper", "socom", "dark cloud", 
+	"kingdom hearts", "beyond good and evil", "suikoden", "amplitude", "psychonauts", "viewtiful joe",
+	"psi ops", "Red Faction", "onimusha", "need for speed", "mercenaries", "hot shots golf", "rogue galaxy",
+	"def jam vendetta", "wipeout", "xmen legends", "tomb raider", "mortal kombat", "legacy of kain",
+	"max payne", "war of the monsters"
+];
+
+var gameChose = "";
+
+var lettersInWord = [];
+
+var blanks = 0;
+
+var blanksAndRights = [];
+
+var wrongs = [];
+
+var wins = 0;
+var losses = 0;
